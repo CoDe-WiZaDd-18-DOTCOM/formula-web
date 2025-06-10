874ac92542cc9454f1658e5fa40481f0a2c279b5
Codewizard-18 <jaswanthm811@gmail.com>
2025-06-10 21:00:33 +0530

    bugs

--- src/helper.tsx
+++ src/helper.tsx
@@ -1,6 +1,2 @@
 export const Domain = import.meta.env.VITE_AUTH0_DOMAIN;
 export const Client = import.meta.env.VITE_AUTH0_CLIENT;
-
-
-// export const Domain = "dev-kchfcftesqkpdp2z.us.auth0.com"
-// export const Client = "HiE7VYK4IIGJCMOq5PAD2aHI5CBRbKz4"
